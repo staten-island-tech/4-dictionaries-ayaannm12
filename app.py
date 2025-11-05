b3f18ef408cd146474165dc39e9ee18072e04dcb
ayaannm12 <ayaanm5352@gmail.com>
2025-11-05 09:35:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-items = {
+item0 = {
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps.",
 }
+item1 = {
+    "name": "Apple Macbook pro 2025  ",
+    "price": 1699.99,
+    "department": "Laptops",
+    "description": "Apple laptop with 16 gb ram."
+}
+item2 = {
 
-print("Here is what we have for sale:", items)
+ "name": "Apple iphone 16 plus",
+        "price": 1000.99,
+        "department":"phones",
+        "Description": "apple iphone 16 plus cellular phone with wifi calling"
 
-want = input("For the Samsung TV, hit 1: ")
+}
+print(f"Here is what we have for sale:{item0} {item1} {item2}")
 
-# Input validation for the first item
-while want != "1":
+want = input("For the Samsung TV, hit 1: For the Apple macbook, hit 2: For the Apple Iphone 16 plus hit 3 ")
+
+while want != "1" "2" "3":
     print("Not a valid answer, please try again.")
-    print("Here is what we have for sale:")
-    print(items)
-    want = input("For the Samsung TV, hit 1: ")
+    print("Here is what we h ave for sale:")
+    print(item0,item1)
+    
+    want = input("For the Samsung TV, hit 1:  For the Apple Macbook pro, hit 2: For the Apple Iphone 16 plus hit 3  ")
 
-print("Added to cart!")
+print("Added to cart!") 
 
-# Ask if they want more
+
 more = input("Do you want anything else? (yes/no): ").lower()
 
-# Keep looping until user says no
 while more != "no":
-    # If it's not yes or no, show error message
+ 
     while more not in ["yes", "no"]:
         print("Not a valid answer, please type 'yes' or 'no'.")
         more = input("Do you want anything else? (yes/no): ").lower()
@@ -31,16 +43,16 @@ while more != "no":
     if more == "no":
         break
 
-    # If yes, repeat the process
-    print("\nHere is what we have for sale again:")
-    print(items)
+    print("Here is what we have for sale again:")
+    print(item0,item1)
 
-    want = input("For the Samsung TV, hit 1: ")
-    while want != "1":
+    want = input("For the Samsung TV, hit 1:  For the Apple macbook, hit 2: For the Apple Iphone 16 plus hit 3  ")
+    while want != "1" "2":
         print("Not a valid answer, please try again.")
-        want = input("For the Samsung TV, hit 1: ")
+        want = input("For the Samsung TV, hit 1: For the Apple macbook, hit 2: For the Apple Iphone 16 plus hit 3 ")
 
     print("Added to cart!")
     more = input("Do you want anything else? (yes/no): ").lower()
 
 print("Thanks for shopping with us!")
+#now find a way to keep track of the cart so the user can go back to take a look at it
